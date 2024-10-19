e19e8007e8fe0672412288204685854191552645
pck-A-pnch <packapunch517@gmail.com>
2024-10-19 15:56:05 +0200

    Csv aggiornato
    + inizio bottoni switch id

--- ScenesAndScripts/TextManager.gd
+++ ScenesAndScripts/TextManager.gd
@@ -2,12 +2,19 @@ extends PanelContainer
 
 @onready var title = $MarginContainer/VBoxContainer/Title
 @onready var description = $MarginContainer/VBoxContainer/Description/Description
+@onready var author = $MarginContainer/VBoxContainer/Author
 
 var file_path = "res://Assets/iChing.csv"
 
 var titles : Array = []
 var descriptions : Array = []
 var ids : Array = []
+var authors : Array = []
+
+var buttonIndexToId : Dictionary = {
+1: Global.id,
+2: Global.id2
+}
 
 func _ready():
 	import_csv()
@@ -23,6 +30,7 @@ func import_csv():
 			titles.append(data_set[3])
 			descriptions.append(data_set[4])
 			ids.append(data_set[1])
+			authors.append(data_set[0])
 		file.close()
 	else:
 		print("File does not exist: " + file_path)
@@ -35,3 +43,4 @@ func _draw():
 			index = i
 	title.text = str(index) + ". " + titles[index]
 	description.text = descriptions[index]
+	author.text = "Scritto da: " + authors[index]
